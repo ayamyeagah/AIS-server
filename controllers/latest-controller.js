dd440c8f5900478e3fb1f5b5adc9bef62169ccf5
ayamyeagah <ahmadalamardiansyah@gmail.com>
2024-05-25 16:10:02 +0700

    modify: latest pipeline

--- controllers/latest-controller.js
+++ controllers/latest-controller.js
@@ -14,56 +14,180 @@ const getLatest = async (req, res) => {
     }
 };
 
+// const pipeline = [
+//     {
+//         '$lookup': {
+//             'from': 'static',
+//             'localField': 'mmsi',
+//             'foreignField': 'mmsi',
+//             'as': 'static_docs'
+//         }
+//     }, {
+//         '$unwind': {
+//             'path': '$static_docs',
+//             'preserveNullAndEmptyArrays': true
+//         }
+//     }, {
+//         '$group': {
+//             '_id': '$mmsi',
+//             'latestDynamic': {
+//                 '$max': {
+//                     '$cond': {
+//                         'if': {
+//                             '$gt': [
+//                                 '$createdAt', '$static_docs.createdAt'
+//                             ]
+//                         },
+//                         'then': '$$ROOT',
+//                         'else': '$$ROOT'
+//                     }
+//                 }
+//             },
+//             'latestStatic': {
+//                 '$max': {
+//                     '$cond': {
+//                         'if': {
+//                             '$lte': [
+//                                 '$createdAt', '$static_docs.createdAt'
+//                             ]
+//                         },
+//                         'then': '$static_docs',
+//                         'else': '$static_docs'
+//                     }
+//                 }
+//             }
+//         }
+//     }, {
+//         '$project': {
+//             '_id': 1,
+//             'dynamic': '$latestDynamic',
+//             'static': '$latestStatic'
+//         }
+//     }
+// ]
+
+const now = new Date();
+const last24Hours = new Date(now.getTime() - 48 * 60 * 60 * 1000);
+
 const pipeline = [
     {
-        '$lookup': {
-            'from': 'static',
-            'localField': 'mmsi',
-            'foreignField': 'mmsi',
-            'as': 'static_docs'
+        $match: {
+            createdAt: {
+                $gte: last24Hours,
+                $lt: now
+            }
         }
-    }, {
-        '$unwind': {
-            'path': '$static_docs',
-            'preserveNullAndEmptyArrays': true
+    },
+    {
+        $sort: {
+            mmsi: -1
         }
-    }, {
-        '$group': {
-            '_id': '$mmsi',
-            'latestDynamic': {
-                '$max': {
-                    '$cond': {
-                        'if': {
-                            '$gt': [
-                                '$createdAt', '$static_docs.createdAt'
-                            ]
-                        },
-                        'then': '$$ROOT',
-                        'else': '$$ROOT'
-                    }
-                }
+    },
+    {
+        $group: {
+            _id: "$mmsi",
+            lon: {
+                $last: "$lon"
             },
-            'latestStatic': {
-                '$max': {
-                    '$cond': {
-                        'if': {
-                            '$lte': [
-                                '$createdAt', '$static_docs.createdAt'
+            lat: {
+                $last: "$lat"
+            },
+            createdAt: {
+                $last: "$createdAt"
+            }
+        }
+    },
+    {
+        $lookup: {
+            from: "static",
+            localField: "_id",
+            foreignField: "mmsi",
+            as: "shipDetails"
+        }
+    },
+    {
+        $unwind: {
+            path: "$shipDetails",
+            preserveNullAndEmptyArrays: true
+        }
+    },
+    {
+        $group: {
+            _id: "$_id",
+            lon: {
+                $first: "$lon"
+            },
+            lat: {
+                $first: "$lat"
+            },
+            createdAt: {
+                $first: "$createdAt"
+            },
+            shipDetails: {
+                $first: "$shipDetails"
+            }
+        }
+    },
+    {
+        $addFields: {
+            name: {
+                $ifNull: ["$shipDetails.name", "Unknown"]
+            },
+            type: {
+                $ifNull: ["$shipDetails.typeAndCargo", 0]
+            },
+            lastReceived: {
+                $let: {
+                    vars: {
+                        diff: {
+                            $divide: [
+                                {
+                                    $subtract: [
+                                        "$$NOW",
+                                        "$createdAt"
+                                    ]
+                                },
+                                60000
                             ]
-                        },
-                        'then': '$static_docs',
-                        'else': '$static_docs'
+                        }
+                    },
+                    in: {
+                        $concat: [
+                            {
+                                $toString: {
+                                    $floor: {
+                                        $divide: ["$$diff", 60]
+                                    }
+                                }
+                            },
+                            " hours, ",
+                            {
+                                $toString: {
+                                    $round: [
+                                        {
+                                            $mod: ["$$diff", 60]
+                                        },
+                                        0
+                                    ]
+                                }
+                            },
+                            " minutes ago"
+                        ]
                     }
                 }
             }
         }
-    }, {
-        '$project': {
-            '_id': 1,
-            'dynamic': '$latestDynamic',
-            'static': '$latestStatic'
+    },
+    {
+        $project: {
+            _id: 1,
+            name: 1,
+            type: 1,
+            lon: 1,
+            lat: 1,
+            lastReceived: 1
         }
     }
-]
+];
 
 module.exports = getLatest;
