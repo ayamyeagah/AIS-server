3ea4ab991f388932920553f42679dc1a67888bd6
ayamyeagah <ahmadalamardiansyah@gmail.com>
2024-07-16 21:16:00 +0700

    fix: query error

--- services/message/updateS-latest.js
+++ services/message/updateS-latest.js
@@ -1,26 +1,34 @@
 const Recents = require('../../models/recents.schema');
 
 const updateLatestStatic = async (message) => {
-    const bulkOpsStatic = message.map(staticUpdate => ({
-        updateOne: {
-            filter: { mmsi: staticUpdate.mmsi },
-            update: {
-                $set: {
-                    static: staticUpdate,
-                    name: staticUpdate.name,
-                    type: staticUpdate.typeAndCargo
-                },
-                $setOnInsert: {
-                    dynamic: {},
-                    coordinates: {
-                        type: "Point",
-                        coordinates: [staticUpdate.lon, staticUpdate.lat]
-                    }
-                }
+    const bulkOpsStatic = message.map(staticUpdate => {
+        const update = {
+
+            $set: {
+                static: staticUpdate,
+                name: staticUpdate.name,
+                type: staticUpdate.typeAndCargo
             },
-            upsert: true
+            $setOnInsert: {
+                dynamic: {}
+            }
+        };
+
+        if (staticUpdate.lon !== null && staticUpdate.lat !== null) {
+            update.$setOnInsert.coordinates = {
+                type: "Point",
+                coordinates: [staticUpdate.lon, staticUpdate.lat]
+            };
         }
-    }));
+
+        return {
+            updateOne: {
+                filter: { mmsi: staticUpdate.mmsi },
+                update: update,
+                upsert: true
+            }
+        };
+    });
 
     await Recents.bulkWrite(bulkOpsStatic);
     // console.log('Updated Static in Latest');
